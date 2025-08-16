4df91717bb48c41e508c562c770656bdfc80110e
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-08-16 23:16:59 +0200

    Moving the "Dashboard" template files from shadcn homepage

---
+++ src/dashboard/page.tsx
@@ -0,0 +1,56 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/dashboard/components/app-sidebar";
+import { ChartAreaInteractive } from "@/dashboard/components/chart-area-interactive";
+import { DataTable } from "@/dashboard/components/data-table";
+import { SectionCards } from "@/dashboard/components/section-cards";
+import { SiteHeader } from "@/dashboard/components/site-header";
+
+import data from "./data.json";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="md:hidden">
+        <img
+          src="/examples/dashboard-light.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <img
+          src="/examples/dashboard-dark.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <SidebarProvider
+        className="hidden md:flex"
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 64)",
+            "--header-height": "calc(var(--spacing) * 12 + 1px)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="sidebar" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                <SectionCards />
+                <div className="px-4 lg:px-6">
+                  <ChartAreaInteractive />
+                </div>
+                <DataTable data={data} />
+              </div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
+}
