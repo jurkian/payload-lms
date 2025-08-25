0e5f001c715e340734eadb910f58c37ce9e1e7a5
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-08-25 22:53:05 +0200

    Remove Payload Cloud

--- back-end/src/payload.config.ts
+++ back-end/src/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -38,7 +37,6 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
 })
