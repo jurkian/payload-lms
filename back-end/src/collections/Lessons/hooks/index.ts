8e3d90e23ffe0d71f34ec165025ec615bf24ae25
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-31 20:50:48 +0100

    Lesson/Course collections: auto create the slug on create/update

--- back-end/src/collections/Lessons/hooks/index.ts
+++ back-end/src/collections/Lessons/hooks/index.ts
@@ -1,13 +1,24 @@
 import { CollectionConfig } from 'payload'
 
 import { deleteDependencies } from './functions/delete-dependencies'
+import { createSlug } from '@utils/text'
 
 const hooks: CollectionConfig['hooks'] = {
-  beforeDelete: [
-    (args) => {
-      return deleteDependencies(args)
-    },
-  ],
+   beforeDelete: [
+      (args) => {
+         return deleteDependencies(args)
+      },
+   ],
+   beforeOperation: [
+      async ({ args, operation }) => {
+         if (operation === 'create' || operation === 'update') {
+            // Update slug
+            args.data.slug = createSlug(args.data.title)
+
+            return args
+         }
+      },
+   ],
 }
 
 export default hooks
