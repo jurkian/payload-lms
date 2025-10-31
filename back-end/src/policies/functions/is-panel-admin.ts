69055ce656807025446deb8e5fdf0709c9ec6a6b
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-31 21:38:03 +0100

    Delete some old files

--- back-end/src/policies/functions/is-panel-admin.ts
+++
@@ -1,9 +0,0 @@
-import type { Access } from 'payload'
-
-export const isPanelAdmin: Access = ({ req: { user } }) => {
-  if (user?.collection === 'admins') {
-    return true
-  }
-
-  return false
-}
