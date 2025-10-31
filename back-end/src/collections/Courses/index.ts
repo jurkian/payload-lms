87f104df104495e6832143dc8c37e7a96bb46f8a
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-31 20:23:17 +0100

    Lessons, courses: add slug and video URL fields

--- back-end/src/collections/Courses/index.ts
+++ back-end/src/collections/Courses/index.ts
@@ -4,33 +4,37 @@ import { groupNames } from '../shared/group-names'
 import hooks from './hooks'
 
 export const Courses: CollectionConfig = {
-  slug: 'courses',
-  admin: {
-    group: groupNames.courses,
-  },
-  fields: [
-    {
-      name: 'title',
-      type: 'text',
-    },
-    {
-      name: 'description',
-      type: 'text',
-    },
-    {
-      name: 'price',
-      type: 'number',
-      min: 1,
-      admin: {
-        step: 1,
+   slug: 'courses',
+   admin: {
+      group: groupNames.courses,
+   },
+   fields: [
+      {
+         name: 'title',
+         type: 'text',
       },
-    },
-    {
-      name: 'createdBy',
-      type: 'relationship',
-      relationTo: 'users',
-    },
-  ],
-  hooks,
-  timestamps: true,
+      {
+         name: 'slug',
+         type: 'text',
+      },
+      {
+         name: 'description',
+         type: 'text',
+      },
+      {
+         name: 'price',
+         type: 'number',
+         min: 1,
+         admin: {
+            step: 1,
+         },
+      },
+      {
+         name: 'createdBy',
+         type: 'relationship',
+         relationTo: 'users',
+      },
+   ],
+   hooks,
+   timestamps: true,
 }
--- back-end/src/collections/Lessons/index.ts
+++ back-end/src/collections/Lessons/index.ts
@@ -4,26 +4,33 @@ import { groupNames } from '../shared/group-names'
 import hooks from './hooks'
 
 export const Lessons: CollectionConfig = {
-  slug: 'lessons',
-  admin: {
-    group: groupNames.lessons,
-  },
-  fields: [
-    {
-      name: 'title',
-      type: 'text',
-    },
-    {
-      name: 'description',
-      type: 'text',
-    },
-    // TODO: video URL
-    {
-      name: 'course',
-      type: 'relationship',
-      relationTo: 'courses',
-    },
-  ],
-  hooks,
-  timestamps: true,
+   slug: 'lessons',
+   admin: {
+      group: groupNames.lessons,
+   },
+   fields: [
+      {
+         name: 'title',
+         type: 'text',
+      },
+      {
+         name: 'slug',
+         type: 'text',
+      },
+      {
+         name: 'description',
+         type: 'text',
+      },
+      {
+         name: 'videoURL',
+         type: 'text',
+      },
+      {
+         name: 'course',
+         type: 'relationship',
+         relationTo: 'courses',
+      },
+   ],
+   hooks,
+   timestamps: true,
 }
