6ef09b8baf22f1350562d6b7d011bdd322e6aade
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-31 23:44:59 +0100

    Fix some old deps

--- back-end/src/collections/Bans/index.ts
+++ back-end/src/collections/Bans/index.ts
@@ -1,64 +1,64 @@
-import { panelAdminsAccess } from '@/policies/panel-admins-access'
+import { panelAdminsAccess } from '@utils/policies/accesses/panel-admins-access'
 import type { CollectionConfig } from 'payload'
 import { groupNames } from '../shared/group-names'
 
 // Single Ban element is just a container for many values related to it
 // it could be a specific registered user, username, ip, email etc.
 export const Bans: CollectionConfig = {
-  slug: 'bans',
-  access: panelAdminsAccess(),
-  admin: {
-    group: groupNames.users,
-  },
-  fields: [
-    {
-      name: 'description',
-      type: 'text',
-    },
-    {
-      name: 'isActive',
-      type: 'checkbox',
-    },
-    {
-      name: 'expiresAt',
-      type: 'date',
-      admin: {
-        date: {
-          pickerAppearance: 'dayAndTime',
-        },
+   slug: 'bans',
+   access: panelAdminsAccess(),
+   admin: {
+      group: groupNames.users,
+   },
+   fields: [
+      {
+         name: 'description',
+         type: 'text',
       },
-    },
-    {
-      name: 'bannedFields',
-      type: 'array',
-      fields: [
-        {
-          name: 'selectedFeature',
-          type: 'select',
-          hasMany: false,
-          admin: {
-            isClearable: true,
-            isSortable: true,
-          },
-          options: ['username', 'ip', 'email'],
-        },
-        {
-          name: 'value',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      name: 'bannedUsers',
-      type: 'array',
-      fields: [
-        {
-          name: 'user',
-          type: 'relationship',
-          relationTo: 'users',
-        },
-      ],
-    },
-  ],
-  timestamps: true,
+      {
+         name: 'isActive',
+         type: 'checkbox',
+      },
+      {
+         name: 'expiresAt',
+         type: 'date',
+         admin: {
+            date: {
+               pickerAppearance: 'dayAndTime',
+            },
+         },
+      },
+      {
+         name: 'bannedFields',
+         type: 'array',
+         fields: [
+            {
+               name: 'selectedFeature',
+               type: 'select',
+               hasMany: false,
+               admin: {
+                  isClearable: true,
+                  isSortable: true,
+               },
+               options: ['username', 'ip', 'email'],
+            },
+            {
+               name: 'value',
+               type: 'text',
+            },
+         ],
+      },
+      {
+         name: 'bannedUsers',
+         type: 'array',
+         fields: [
+            {
+               name: 'user',
+               type: 'relationship',
+               relationTo: 'users',
+            },
+         ],
+      },
+   ],
+   timestamps: true,
 }
